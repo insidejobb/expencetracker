6ab55f7321460538e7efa8544bf52365086810df
Bhavani <bhavanii1924@gmail.com>
2023-04-04 23:21:37 +0530

    expencet

---
+++ src/components/Expenceitem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return  <div>
+        
+        <h2>Food Rs 10</h2>
+        <h4>Petrol Rs 100</h4>
+         <h5>Movies Rs 100</h5> 
+     </div>       
+}
+
+export default ExpenseItem;
