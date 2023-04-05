7bd33524e9a90f1c86cfc1750a0a8d25317f5fee
Bhavani <bhavanii1924@gmail.com>
2023-04-05 22:01:16 +0530

    looping of expenses

--- src/components/Expenceitem.js
+++ src/components/Expenceitem.js
@@ -1,10 +1,17 @@
-function ExpenseItem(){
-    return  <div>
-        
-        <h2>Food Rs 10</h2>
-        <h4>Petrol Rs 100</h4>
-         <h5>Movies Rs 100</h5> 
-     </div>       
+import './Expenceitem.css';
+
+function ExpenseItem(props) {
+    return (
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+                <div>{props.location}</div>
+            </div>
+        </div>
+    );
 }
+
 
 export default ExpenseItem;
