f465e2dd0bcb30f551cfe34d8b283916051fe1fd
Bhavani <bhavanii1924@gmail.com>
2023-04-16 00:08:21 +0530

    expenseFilter using

--- src/components/EXPENSES/expenseitem.js
+++ src/components/EXPENSES/expenseitem.js
@@ -1,26 +1,16 @@
-import React,{ useState} from 'react';
 import ExpenseDate from './expenseDate';
 import Card from '../UI/Card';
 import './expenseitem.css';
 
-const ExpenseItem=(props)=> {
-   const [amount,setAmount]= useState(props.amount);
-   console.log('ExpenseItem evaluated by React')
-
-    const clickHandler=()=>{
-        setAmount('100'); 
-       console.log(amount);
-    };
-    
+const ExpenseItem=(props)=> { 
     return (
         
             <Card className="expense-item">
                 <ExpenseDate date={props.date} />
                 <div className='expense-item__description'>
                     <h2>{props.title}</h2>
-                    <div className='expense-item__price'>${amount}</div>
+                    <div className='expense-item__price'>${props.amount}</div>
                 </div>
-                <button onClick={clickHandler}>Change Title</button>
             </Card>
         
     );
